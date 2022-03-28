7813c1175e546f6f869d8c765c53f332b781d03d
soccerjesse <jishperu10@gmail.com>
2022-03-29 01:03:54 +0900

    login form

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   root 'homes#top'
   
   resource :user, only: [:new, :create, :show]
+
+  get    'login', to: 'sessions#new'
+  post   'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
 end
