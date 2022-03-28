337c2ca5e033fa1a34027294fb67023f64a6f040
soccerjesse <jishperu10@gmail.com>
2022-03-28 22:42:12 +0900

    Sign up create

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  def new  #登録ページ
+    @user = User.new
+  end
+
+  def show #詳細ページ
+    
+  end
+
+  def create #post 
+    user = User.create(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to user_path, notice: "create account!!"
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
+end
